64a71637a15b9ed77b29d07eb69855648b0f0725
ajayhedau07 <ajayhedau455@gmail.com>
2025-10-10 15:01:51 +0530

    user port

--- src/main/java/com/skilluser/user/UserApplication.java
+++ src/main/java/com/skilluser/user/UserApplication.java
@@ -13,12 +13,12 @@ public class UserApplication {
 	public static void main(String[] args) {
 
 
-		Dotenv dotenv = Dotenv.load(); // loads .env file automatically
+		/*Dotenv dotenv = Dotenv.load(); // loads .env file automatically
 
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USER", dotenv.get("DB_USER"));
 		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));*/
 
 
 		SpringApplication.run(UserApplication.class, args);
