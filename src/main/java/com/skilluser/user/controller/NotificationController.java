58ee4afaec0eec42ad6afc1a36368c6215e16010
Ashvin-chopkar1 <ashvin_chopkar@skillpilots.com>
2025-10-29 11:21:59 +0530

    notification

--- src/main/java/com/skilluser/user/controller/NotificationController.java
+++ src/main/java/com/skilluser/user/controller/NotificationController.java
@@ -3,9 +3,12 @@ package com.skilluser.user.controller;
 import com.skilluser.user.model.Notification;
 import com.skilluser.user.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 // 28/10/2025 - Ashvin Chopkar
 @RestController
@@ -16,25 +19,30 @@ public class NotificationController {
 
     // get user notification by user id
     @GetMapping("/{userId}")
-    public List<Notification> getUserNotifications(@PathVariable Long userId) {
-        return notificationRepository.findByReceiverIdOrderByCreatedAtDesc(userId);
+    public ResponseEntity<List<Notification>> getUserNotifications(@PathVariable Long userId) {
+        List<Notification> notifications = notificationRepository.findByReceiverIdOrderByCreatedAtDesc(userId);
+        return ResponseEntity.ok(notifications);
     }
 
 
 
     // get all unread notifications
     @GetMapping("/unread/{userId}")
-    public List<Notification> getUnreadNotifications(@PathVariable Long userId) {
-        return notificationRepository.findByReceiverIdAndReadStatusFalseOrderByCreatedAtDesc(userId);
+    public ResponseEntity<List<Notification>> getUnreadNotifications(@PathVariable Long userId) {
+        List<Notification> notifications = notificationRepository.findByReceiverIdAndReadStatusFalseOrderByCreatedAtDesc(userId);
+        return ResponseEntity.ok(notifications);
     }
 
 
     // update read status when user seen notification
-    @PutMapping("/{id}/read")
-    public void markAsRead(@PathVariable Long id) {
+    @PutMapping("/read/{id}")
+    public ResponseEntity<Map<String,String>> markAsRead(@PathVariable Long id) {
+        Map<String,String> responce = new HashMap<>();
         Notification notification = notificationRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Notification not found"));
         notification.setReadStatus(true);
         notificationRepository.save(notification);
+        responce.put("message", "user seen notification successfully!");
+        return ResponseEntity.ok(responce);
     }
 }
