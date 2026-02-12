eb62cae9ff587e8b84f10e8d9a0f87219019109b
AkshayG079 <gedamakshay150@gmail.com>
2026-02-12 10:31:00 +0530

    user

--- src/main/java/com/skilluser/user/repository/NotificationRepository.java
+++ src/main/java/com/skilluser/user/repository/NotificationRepository.java
@@ -2,7 +2,11 @@ package com.skilluser.user.repository;
 
 import com.skilluser.user.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +17,14 @@ public interface NotificationRepository extends JpaRepository<Notification,Long>
     List<Notification> findByReceiverIdAndReadStatusFalseOrderByCreatedAtDesc(Long receiverId);
 
     Optional<Notification> findByNotificationIdAndReceiverId(Long notificationId, Long receiverId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value =
+            "DELETE FROM notification WHERE created_at < :date",
+            nativeQuery = true)
+    int deleteAllOld(@Param("date") LocalDateTime date);
+
+
+
 
 }
